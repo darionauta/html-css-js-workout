b80752627654eee1fe7d94926914be1f4fe0134e
darionauta <dariusz.stanek@gmail.com>
2023-07-30 15:42:36 +0200

    border color changer

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('Hello from Nerdbord!')
+
+var colors = [ 'red', 'yellow', 'green', 'blue'];
+
+var el = document.querySelector(".box");
+var counter = 0;
+
+(function(){
+    setInterval(function(){
+        el.style.setProperty("border-color", colors[counter]);
+        counter = counter > colors.length - 1 ? 0 : counter + 1; 
+    }, 3000);
+})()
